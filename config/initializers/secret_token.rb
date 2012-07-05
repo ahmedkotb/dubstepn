c4e53d83b62d2cff810e04a7d91129b00eb6f354
Stephan Boyer <boyerstephan@gmail.com>
2012-07-04 19:37:53 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dubstepn::Application.config.secret_token = '024987b60c4404a1420d3420b724a21e9a1321e25f7b27a40a49074282c993abec620304fec13e6432a65a219a6d4a0634a5a574c242c719a3997bbce2a43402'
