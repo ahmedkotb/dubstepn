4fdd457f960baed2ba4372df1f2c2d704175e4e3
Stephan Boyer <boyerstephan@gmail.com>
2012-07-07 17:26:43 -0700

    markdown test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
+require 'tempfile'
+
 class HomeController < ApplicationController
   def index
+    file = Tempfile.new "markdown"
+    file.write("hello world")
+    file.close
+    @test = `perl ./script/markdown.pl #{file.path}`
+    file.unlink
   end
 end
