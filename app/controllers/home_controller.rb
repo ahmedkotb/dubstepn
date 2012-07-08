5cf1655c318df608eac69445251f9343aa49fa06
Stephan Boyer <boyerstephan@gmail.com>
2012-07-07 17:33:26 -0700

    adding markdown functionality to helpers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,6 @@
-require 'tempfile'
+include ApplicationHelper
 
 class HomeController < ApplicationController
   def index
-    file = Tempfile.new "markdown"
-    file.write("hello world")
-    file.close
-    @test = `perl ./script/markdown.pl #{file.path}`
-    file.unlink
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
+require 'tempfile'
+
 module ApplicationHelper
+  def markdown(s)
+    file = Tempfile.new "markdown"
+    file.write(s)
+    file.close
+    result = `perl ./script/markdown.pl #{file.path}`
+    file.unlink
+    return result
+  end
 end
