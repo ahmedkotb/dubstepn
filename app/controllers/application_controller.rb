61f87c6e25438f722108e8f989219ae781a7d1e6
Stephan Boyer <boyerstephan@gmail.com>
2012-07-08 17:57:36 -0700

    removing broken 404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  def render_optional_error_file(status_code)
-    if status_code == :not_found
-      render_404
-    else
-      super
-    end
-  end
-
-private
-  def render_404
-    respond_to do |type| 
-      type.html { render :template => "home/notfound", :layout => "application", :status => 404 } 
-      type.all  { render :nothing => true, :status => 404 } 
-    end
-    true
-  end
-
 end
