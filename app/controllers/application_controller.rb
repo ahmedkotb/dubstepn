5e3dd7c011a19c8e939c0615031f1304bc2884a9
Stephan Boyer <boyerstephan@gmail.com>
2012-07-11 03:36:03 -0700

    tag cloud

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,10 @@ class ApplicationController < ActionController::Base
   private
     def get_layout_fields
       @posts = Post.order("created_at DESC").all
+      @tags_hash = @posts.map { |post| post.tags.split(",").map { |tag| tag.strip } }.flatten.inject(Hash.new(0)) { |h, x|
+        h[x] += 1
+        h
+      }
+      @sorted_tags = @tags_hash.keys.sort { |a, b| @tags_hash[a] <=> @tags_hash[b] }
     end
 end
