9c97ba2201b51fac00068423c9eab44096d8e799
Stephan Boyer <boyerstephan@gmail.com>
2012-08-21 13:32:52 -0700

    more stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,9 @@ class ApplicationController < ActionController::Base
   private
     def get_layout_fields
       @recent_posts = Post.order("id DESC").select { |post| post.tags.map{ |tag| tag.name }.include?("home") }.first(5)
+      @sidebar_posts = Post.order("id DESC").select { |post| post.tags.map{ |tag| tag.name }.include?("sidebar") }
+      for post in @sidebar_posts
+        post.content = markdown(post.content).gsub("<pre><code>", "<pre class=\"brush: python; toolbar: false;\">").gsub("</code></pre>", "</pre>").gsub("<h6>", "<p>").gsub("</h6>", "</p>").gsub("<h5>", "<p>").gsub("</h5>", "</p>").gsub("<h4>", "<h6>").gsub("</h4>", "</h6>").gsub("<h3>", "<h5>").gsub("</h3>", "</h5>").gsub("<h2>", "<h4>").gsub("</h2>", "</h4>").gsub("<h1>", "<h3>").gsub("</h1>", "</h3>")
+      end
     end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -22,7 +22,6 @@ class HomeController < ApplicationController
       return backtrack("login")
     end
     @post.content = markdown(@post.content).gsub("<pre><code>", "<pre class=\"brush: python; toolbar: false;\">").gsub("</code></pre>", "</pre>").gsub("<h6>", "<p>").gsub("</h6>", "</p>").gsub("<h5>", "<p>").gsub("</h5>", "</p>").gsub("<h4>", "<h6>").gsub("</h4>", "</h6>").gsub("<h3>", "<h5>").gsub("</h3>", "</h5>").gsub("<h2>", "<h4>").gsub("</h2>", "</h4>").gsub("<h1>", "<h3>").gsub("</h1>", "</h3>")
-    @logged_in = is_logged_in
   end
 
   def resume
