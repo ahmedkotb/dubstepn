77082fc12b9569adc9d048f1f4c63c996030e970
Stephan Boyer <boyerstephan@gmail.com>
2012-08-23 04:40:30 -0700

    bugfix

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Dubstepn::Application.routes.draw do
   # make sure all routes have www
   constraints(:host => "stephanboyer.com") do
     match "/" => redirect("http://www.stephanboyer.com")
-    match "/*path" => redirect { |params| "http://www.stephanboyer.com#{request.fullpath}" }
+    match ("/*path" => redirect { |params| "http://www.stephanboyer.com/#{params[:path]}" }), :format => false
   end
 
   # public routes
