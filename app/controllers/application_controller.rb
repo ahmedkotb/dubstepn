57bf17f51404b03b1524cd9872b29692e52d88c8
Stephan Boyer <boyerstephan@gmail.com>
2012-08-24 12:37:54 -0700

    tiny optimization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,8 +16,5 @@ class ApplicationController < ActionController::Base
         @recent_posts = if home_tag then home_tag.posts.where(:is_public => true).order("sort_id DESC").limit(5) else [] end
         @sidebar_posts = if sidebar_tag then sidebar_tag.posts.where(:is_public => true).order("sort_id DESC") else [] end
       end
-      for post in @sidebar_posts
-        post.content = markdown(post.content).gsub("<pre><code>", "<pre class=\"brush: python; toolbar: false;\">").gsub("</code></pre>", "</pre>").gsub("<h6>", "<p>").gsub("</h6>", "</p>").gsub("<h5>", "<p>").gsub("</h5>", "</p>").gsub("<h4>", "<h6>").gsub("</h4>", "</h6>").gsub("<h3>", "<h5>").gsub("</h3>", "</h5>").gsub("<h2>", "<h4>").gsub("</h2>", "</h4>").gsub("<h1>", "<h3>").gsub("</h1>", "</h3>")
-      end
     end
 end
