7a1fb86be7c06d4fe047be505f7c886cfcf79463
Stephan Boyer <boyerstephan@gmail.com>
2013-04-09 23:09:56 -0400

    fixing handling of quotes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,37 @@ module ApplicationHelper
     file.close
     result = `perl ./script/markdown.pl #{file.path}`
     file.unlink
+
+    def char_is_raw(str, pos)
+      left = str[0...pos]
+      pre_open = left.scan(/\<pre\>/).length
+      code_open = left.scan(/\<code\>/).length
+      pre_close = left.scan(/\<\/pre\>/).length
+      code_close = left.scan(/\<\/code\>/).length
+      math_open = left.scan(/\\\(/).length
+      math_close = left.scan(/\\\)/).length
+      tag_open = left.scan(/\</).length
+      tag_close = left.scan(/\>/).length
+      return pre_open <= pre_close && code_open <= code_close && math_open <= math_close && tag_open <= tag_close
+    end
+
+    pos = result.length - 1
+    double_quote_parity = true
+    while pos >= 0
+      if result[pos] == "\'" && char_is_raw(result, pos)
+        result = result[0...pos] + "&rsquo;" + result[(pos + 1)..result.length]
+      end
+      if result[pos] == "\"" && char_is_raw(result, pos)
+        if double_quote_parity
+          result = result[0...pos] + "&rdquo;" + result[(pos + 1)..result.length]
+        else
+          result = result[0...pos] + "&ldquo;" + result[(pos + 1)..result.length]
+        end
+        double_quote_parity = !double_quote_parity
+      end
+      pos -= 1
+    end
+
     result.gsub!("<pre><code>", "<pre class=\"brush: plain; toolbar: false;\">")
     result.gsub!("</code></pre>", "</pre>")
     result.gsub!("<h6>",  "<p>")
@@ -21,6 +52,7 @@ module ApplicationHelper
     result.gsub!("</h2>", "</h4>")
     result.gsub!("<h1>",  "<h3>")
     result.gsub!("</h1>", "</h3>")
+
     return result
   end
 
