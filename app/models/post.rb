e5fe8b139f863c99265366d17fb57e5806668be9
Stephan Boyer <boyerstephan@gmail.com>
2013-04-14 23:42:11 -0400

    adding descriptions to each page

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,40 @@ class Post < ActiveRecord::Base
   has_and_belongs_to_many :tags
   has_many :children, :class_name => "Post", :foreign_key => "parent_id"
   belongs_to :parent, :class_name => "Post"
+
+  # get a short summary of this post for SEO
+  def summary
+    # start with the markdown
+    summary = self.content
+
+    # remove html tags
+    summary.gsub!(/\<[^>]*\>/, "")
+
+    # remove titles
+    summary.gsub!(/^\#.*$/, " ")
+
+    # escape sequences
+    summary.gsub!(/\\\(/, "(")
+    summary.gsub!(/\\\)/, ")")
+    summary.gsub!(/\\\\/, "\\")
+
+    # remove math markup
+    summary.gsub!(/\\\( *([^\\ ]*) *\\\)/, "\\1")
+
+    # replace hyperlinks with their text
+    summary.gsub!(/\[([^\]]*[^\\])\]\([^\)]*[^\\]\)/, "\\1")
+
+    # remove leading and trailing whitespace
+    summary.strip!
+
+    # take the first 155 characters
+    if summary.length > 155
+      summary = summary.first(155).split.reverse.drop(1).reverse.join(" ") + "..."
+    else
+      summary = summary.split.join(" ")
+    end
+
+    # return the result
+    return summary
+  end
 end
