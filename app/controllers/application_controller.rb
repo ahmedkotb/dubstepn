482eff00cc56d654d29a6137aef153fa5012729e
Stephan Boyer <boyerstephan@gmail.com>
2013-04-29 01:17:39 -0400

    recent posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,11 +15,12 @@ class ApplicationController < ActionController::Base
     def get_layout_fields
       home_tag = Tag.where(:name => "home").first
       sidebar_tag = Tag.where(:name => "sidebar").first
+      num_recent_posts = 5
       if is_logged_in
-        @recent_posts = if home_tag then home_tag.posts.order("sort_id DESC").limit(5) else [] end
+        @recent_posts = if home_tag then home_tag.posts.order("sort_id DESC").limit(num_recent_posts) else [] end
         @sidebar_posts = if sidebar_tag then sidebar_tag.posts.order("sort_id DESC") else [] end
       else
-        @recent_posts = if home_tag then home_tag.posts.where(:is_public => true).order("sort_id DESC").limit(5) else [] end
+        @recent_posts = if home_tag then home_tag.posts.where(:is_public => true).order("sort_id DESC").limit(num_recent_posts) else [] end
         @sidebar_posts = if sidebar_tag then sidebar_tag.posts.where(:is_public => true).order("sort_id DESC") else [] end
       end
     end
