025ab84a5c8e1232612b7ef620b014d8d52c6ac2
Stephan Boyer <boyerstephan@gmail.com>
2013-09-11 03:38:03 -0400

    first version of link tester

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,5 @@
+require 'net/http'
+require 'socket'
 require 'tempfile'
 
 module ApplicationHelper
@@ -6,9 +8,18 @@ module ApplicationHelper
   APP_TITLE = "Stephan Boyer"
   APP_AUTHOR = "Stephan Boyer"
   APP_EMAIL = "boyers@mit.edu"
-  APP_HOST = "www.stephanboyer.com"
+  if Rails.env.production?
+    APP_HOST = "www.stephanboyer.com"
+  else
+    APP_HOST = "localhost:3000"
+  end
   APP_RESUME_URL = "http://s3.amazonaws.com/dubstepn/resume.pdf"
 
+  # converts markdown into html
+  # does some extra processing:
+  # - replaces ' and " with their directional counterparts
+  # - replaces <pre><code>...</code></pre> with <pre>...</pre> (inline code gets put in <code> tags whereas block code gets put in <pre> tags)
+  # - shifts all headings by 2 (so h1 -> h3, h2 -> h4, etc.)
   def markdown(s)
     file = Tempfile.new "markdown"
     file.write(s)
@@ -78,6 +89,7 @@ module ApplicationHelper
     return result
   end
 
+  # call this if the markdown(...) function above ever changes to re-markdown all existing posts
   def remarkdown_all_posts!
     for post in Post.all
       post.content_html = markdown(post.content)
@@ -85,6 +97,116 @@ module ApplicationHelper
     end
   end
 
+  # this function crawls the website and tries to follow every link
+  # it prints the results to stdout and returns nothing
+  # don't call this from within a web request or you'll get an infinite loop because
+  # this function will try to make a request but the server won't be able to handle it
+  # since it's already handling the request that invoked this
+  def test_links
+    # URLs to crawl
+    # each item in this list is of the form [url, referrer]
+    # referrer is nil for the root
+    agenda = [["/", nil]]
+
+    # URLs we have crawled already
+    # each item in this list is of the form [url, referrer, error]
+    # error is nil on success
+    visited = []
+
+    # crawl the website
+    while agenda.size > 0
+      # take a URL from the agenda
+      pair = agenda.pop
+      url = pair[0]
+      referrer = pair[1]
+      print "."
+
+      # get a complete version of the URL
+      full_url = url
+      if full_url.size == 0
+        full_url = "/"
+      end
+      if full_url.start_with?("/")
+        full_url = APP_HOST + full_url
+      end
+      if !full_url.start_with?("http://")
+        full_url = "http://" + full_url
+      end
+
+      # try to fetch the URL
+      begin
+        uri = URI(full_url)
+        res = nil
+        Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == "https", :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
+          request = Net::HTTP::Get.new(uri)
+          res = http.request(request)
+        end
+      rescue Exception => e
+        visited.push([url, referrer, e.to_s])
+        next
+      end
+
+      # check the response code
+      if res.code != "200"
+        visited.push([url, referrer, "response code: " + res.code])
+        next
+      end
+
+      # for local pages, parse the body for hyperlinks
+      if url.start_with?("/")
+        new_urls = []
+        new_urls += res.body.scan(/\<a.*href\=\"([^"]*)\".*\>[^<]*\<\/a\>/).map { |result| result[0] }
+        new_urls += res.body.scan(/\<a.*href\=\'([^']*)\'.*\>[^<]*\<\/a\>/).map { |result| result[0] }
+        for new_url in new_urls
+          if new_url == url
+            next
+          end
+          if agenda.map { |result| result[0] }.include?(new_url)
+            next
+          end
+          if visited.map { |result| result[0] }.include?(new_url)
+            next
+          end
+          agenda.push([new_url, url])
+        end
+      end
+
+      # add the url to the visited list and continue
+      visited.push([url, referrer, nil])
+    end
+    puts ""
+    puts ""
+
+    # print the successful urls
+    puts "Successful URLs:"
+    puts ""
+    for item in visited.sort { |a, b| a[0] <=> b[0] }
+      if item[2] == nil
+        if item[1] == nil
+          puts "* " + item[0]
+        else
+          puts "* " + item[0] + " from " + item[1]
+        end
+      end
+    end
+    puts ""
+
+    # print the failed urls
+    puts "Failed URLs:"
+    puts ""
+    for item in visited.sort { |a, b| a[0] <=> b[0] }
+      if item[2] != nil
+        if item[1] == nil
+          puts "* " + item[0] + ": " + item[2]
+        else
+          puts "* " + item[0] + " from " + item[1] + ": " + item[2]
+        end
+      end
+    end
+    return
+  end
+
+  # return whether the user is logged in
   def is_logged_in
     if session[:login_time] == nil
       return false
